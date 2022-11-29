3edc5aabd92c6ac0379a53eec8c7cf16fbbb15a3
CCWink <switchwink@163.com>
2022-11-29 15:54:59 +0800

    fourth commit

--- src/main/java/com/csz/git/GitTest.java
+++ src/main/java/com/csz/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
 		System.out.println("这是我的第一个Git项目 版本一");
 		System.out.println("这是我的第一个Git项目 版本二");
 		System.out.println("这是我的第一个Git项目 版本三");
+		System.out.println("这是我的第一个Git项目 版本四");
 	}
 }
