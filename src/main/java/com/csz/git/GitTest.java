10e9c7e967c8e1654e48819fe1b7ca60e3bef516
CCWink <switchwink@163.com>
2022-11-29 15:45:27 +0800

    first commit

---
+++ src/main/java/com/csz/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.csz.git;
+
+/**
+ * @author: CCWink
+ * @Date: 2022/11/29
+ */
+
+public class GitTest {
+	public static void main(String[] args) {
+		System.out.println("这是我的第一个Git项目");
+	}
+}
